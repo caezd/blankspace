7c64268939705dec9ac123aff24d402ce36f8dcc
Monomer <caedrikbl@gmail.com>
2025-10-14 22:58:27 -0400

    source from search

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,7 +1,7 @@
-import { source } from "@/lib/source";
+import { theme } from "@/lib/source";
 import { createFromSource } from "fumadocs-core/search/server";
 
-export const { GET } = createFromSource(source, {
+export const { GET } = createFromSource(theme, {
     // https://docs.orama.com/docs/orama-js/supported-languages
     language: "french",
 });
